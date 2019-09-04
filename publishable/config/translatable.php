a35781c834a651bc192a758062eb0b17124145d8
Sertxu Developer <dev.sertxu@gmail.com>
2019-09-04 22:26:25 +0200

    Create translatable.php

---
+++ publishable/config/translatable.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /**
+     * The locales availables in your project.
+     */
+    "locales" => [],
+    
+    /**
+     * The default locale in your project, it will be used if the requested locale is not available.
+     */
+    "fallback_locale" => "en",
+    
+    /**
+     * Hide the default locale from the URL.
+     */
+    "hide_fallback_locale" => true,
+];
