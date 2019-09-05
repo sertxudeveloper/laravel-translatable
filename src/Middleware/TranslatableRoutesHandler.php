531b06dc8aa483ba6a54829b2a8d69eb6a5afd92
Sertxu Developer <dev.sertxu@gmail.com>
2019-09-05 04:37:16 +0200

    Add files via upload

---
+++ src/Middleware/TranslatableRoutesHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SertxuDeveloper\Translatable\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Closure;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+use SertxuDeveloper\Translatable\Facades\Translatable;
+use Illuminate\Http\RedirectResponse;
+
+class TranslatableRoutesHandler {
+
+  /**
+   * Set language for localized route
+   *
+   * @param \Illuminate\Http\Request $request
+   * @param \Closure $next
+   * @return mixed
+   */
+  public function handle(Request $request, Closure $next) {
+    $params = explode('/', $request->getPathInfo());
+    // Dump the first element (empty string) as getPathInfo() always returns a leading slash
+    array_shift($params);
+
+    if (\count($params) > 0) {
+      $locale = $params[0];
+      if (Translatable::checkLocaleInSupportedLocales($locale)) {
+        App::setLocale($locale);
+        if (Translatable::isFallbackLocaleHidden() && Translatable::isFallbackLocale($locale)) {
+          array_shift($params);
+          $params = implode('/', $params);
+          $url = $request->server('REQUEST_SCHEME') . '://' . $request->server('HTTP_HOST') . '/' . $params;
+          Session::reflash();
+          return new RedirectResponse($url, 302, ['Vary' => 'Accept-Language']);
+        }
+      }
+    }
+
+    return $next($request);
+  }
+}
